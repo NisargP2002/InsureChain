942642fbcdd126ecfead49d383314ec4864cedf5
Nisarg Pansare <115652999+NisargP2002@users.noreply.github.com>
2024-01-16 11:21:02 +0530

    Update Insurance.sol

--- Insurance.sol
+++ Insurance.sol
@@ -11,6 +11,7 @@ contract InsurancePlatform {
 
     event PolicyPurchased(address indexed buyer, uint256 amount, uint256 expiration);
     event ClaimFiled(address indexed claimant, uint256 amount);
+    event TransferToPolicyHolder(address indexed policyHolder, uint256 amount);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
@@ -35,12 +36,21 @@ contract InsurancePlatform {
         owner = msg.sender;
         premiumAmount = _premiumAmount;
         coverageAmount = _coverageAmount;
-        policyDuration = _policyDurationDays * 1 days;
+        policyDuration = _policyDurationDays * 1 minutes;
+    }
+
+    receive() external payable {
+        // This function is called when Ether is sent directly to the contract
+        // It transfers the received Ether to the owner and deducts it from the owner's balance
+        payable(owner).transfer(msg.value);
     }
 
     function purchasePolicy() external payable {
-        require(msg.value == premiumAmount, "Incorrect premium amount sent");
+        require(msg.value == premiumAmount * 1 ether, "Incorrect premium amount sent");
         require(policyHolders[msg.sender] == 0, "Policy already purchased");
+
+        // Transfer the premium amount in Ether to the owner
+        payable(owner).transfer(msg.value);
 
         uint256 expirationTimestamp = block.timestamp + policyDuration;
         policyHolders[msg.sender] = expirationTimestamp;
@@ -48,11 +58,16 @@ contract InsurancePlatform {
         emit PolicyPurchased(msg.sender, msg.value, expirationTimestamp);
     }
 
-    function fileClaim() external onlyPolicyHolder hasNotClaimed {
-        require(block.timestamp > policyHolders[msg.sender], "Policy has not expired");
+    function transferToPolicyHolder(address policyHolder) external onlyOwner payable {
+        require(policyHolders[policyHolder] > 0, "Invalid policyholder address");
 
-        hasClaimed[msg.sender] = true;
+        // Deduct the coverageAmount in Ether from the owner's external wallet
+        // require(owner.balance >= coverageAmount * 1 ether, "Insufficient funds in owner's wallet");
 
-        emit ClaimFiled(msg.sender, coverageAmount);
+        // Transfer the coverageAmount in Ether to the policyholder
+        payable(policyHolder).transfer(2 ether);
+
+        emit TransferToPolicyHolder(policyHolder, 2 ether);
     }
+
 }
